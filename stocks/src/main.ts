7a4c84c0ad522b05eaa2e4dd06ea47d7bc31ec1a
Felly KANYIKI <fellyn@mintek.co.za>
2025-03-12 12:26:47 +0200

    Modified base scaffolding to have Material Design Lite markup

--- stocks/src/main.ts
+++ stocks/src/main.ts
@@ -1,6 +1,8 @@
+//Imports dependencies
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+//Bootstraps the App module
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
