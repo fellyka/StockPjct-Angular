e7b97090222a1ae80d9359ae5ff3a800b58b5a69
Felly KANYIKI <fellyn@mintek.co.za>
2025-03-12 14:49:39 +0200

    Creating components/summary & updating theapp.module

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core'; //Imports Angular dependencies needed
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { StocksService } from './services/stocks.service';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; //Imports the App component
+import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component'; //Imports the App component
 
 //Uses the NgModule annotation to define a module by passing an object
 @NgModule({
   //Declarations are to list any components and directives in the app
-  declarations: [AppComponent],
+  declarations: [AppComponent, SummaryComponent],
 
-  //Imports are other modules that are used in the app
-  imports: [BrowserModule, AppRoutingModule],
+  //Imports other modules that are used in the app
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
 
   //Services will be listed here
-  providers: [],
+  providers: [StocksService],
 
   //Bootstrap declares which component to use as the first to bootstrap the application
   bootstrap: [AppComponent],
--- stocks/src/app/services/stocks.service.ts
+++ stocks/src/app/services/stocks.service.ts
@@ -1,11 +1,51 @@
 //Imports dependencies
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+//Declares a stock array and API variables
+let stocks: Array<string> = ['APPL', 'GOOG','FB','AMZN','TWTR'];
+let service : string = 'https://angular2-in-action-api.herokuapp.com';
 
+//Defines and exports the TpeScript interface for a stock object
+export interface StockInterface{
+   symbol:string;
+   lastTradePriceOnly: number;
+   change: number;
+   changeInPercent: number;
+}
+
+//Annotates with Injectable to wire up dependency injection
 @Injectable({
   providedIn: 'root'
 })
-export class StocksService {
 
-  constructor() { }
+//Defines class and exports it
+export class StocksService {
+  //Constructor method to inject HttpClient service into class http property
+  constructor(private http: HttpClient) { 
+
+  }
+
+  //Method to get the stocks
+  get(){
+    return stocks.slice();
+  }
+
+  //Method to add new stock list
+  add(stock: any){
+    stocks.push(stock);
+    return this.get();
+  }
+  //Method to remove a stock from list
+  remove(stock : any){
+    stocks.splice(stocks.indexOf(stock),1);
+    return this.get()
+  }
+
+  //Method to call HttpClient service to load stock values from API
+  load(symbols : any){
+    if(symbols){
+      return this.http.get<Array<StockInterface>>(service + '/stocks/snapshots?symbols=' + symbols.join())
+    }
+  }
 }
