664cc8438ce17f8615648501aa193a1a5b06ac2c
Felly KANYIKI <fellyn@mintek.co.za>
2025-03-12 11:54:57 +0200

    How Angular works

--- stocks/src/app/app.component.ts
+++ stocks/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //Import the component annotation
 
+//Define the component and its properties
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
+
+//Create the component controller, with a single property
 export class AppComponent {
-  title = 'stocks';
+  title = 'app works';
 }
