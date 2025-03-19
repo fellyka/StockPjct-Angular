dff9a352a3da786b366e154a2a711ff9ff1f3ce6
Felly KANYIKI <fellyn@mintek.co.za>
2025-03-19 11:19:32 +0200

    manage component & the FormsModule added

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core'; //Imports Angular dependencies needed
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { StocksService } from './services/stocks.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
-import { DashbordComponent } from './components/dashbord/dashbord.component'; 
+import { DashbordComponent } from './components/dashbord/dashbord.component';
+import { ManageComponent } from './components/manage/manage.component'; 
 
 //Uses the NgModule annotation to define a module by passing an object
 @NgModule({
   //Declarations are to list any components and directives in the app
-  declarations: [AppComponent, SummaryComponent, DashbordComponent],
+  declarations: [AppComponent, SummaryComponent, DashbordComponent, ManageComponent],
 
   //Imports other modules that are used in the app
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
 
   //Services will be listed here
   providers: [StocksService],
